88fc98e42ce3be4172a319eaa729b824fb5ba333
Prasad Nayak <prasahdk9@gmail.com>
2024-12-24 17:25:02 +0530

    a

---
+++ src/test/kotlin/com/ms/service_rergistry/ServiceRergistryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ms.service_rergistry
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServiceRergistryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
